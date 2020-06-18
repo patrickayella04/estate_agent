37893e6bdf87775cc1c04c99ab321005793fc148
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-06-18 12:44:26 +0100

    navi responsive addition

---
+++ estate_agent_site/app.js
@@ -0,0 +1,14 @@
+
+const can = document.querySelector('.can');
+
+const arrow = document.querySelector('.arrow-dropdown')
+
+
+arrow.addEventListener('click', function () {
+
+    
+        can.style.display = 'none';
+    
+    
+});
+
