9d40adcf7ddcc8bf4d9a1901d814cc663bb2b891
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-06-22 18:28:17 +0100

    dropdown cananda option update

--- estate_agent_site/app.js
+++ estate_agent_site/app.js
@@ -1,16 +1,15 @@
 
 const can = document.querySelector('.can');
-
 const arrow = document.querySelector('.arrow-dropdown');
-
 const uk = document.querySelector('.country-option');
-
 const jack = document.querySelector('.jack');
+
 
 arrow.addEventListener('click', openNav);
 uk.addEventListener('click', openNav);
 jack.addEventListener('click', openNav);
-        
+    
+
 function openNav() {
         can.classList.toggle('show');
 };
