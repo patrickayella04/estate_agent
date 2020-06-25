55763829a464ad22fc6af9686288a1a8f60df3a6
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-06-25 17:19:43 +0100

    toggle-dropNavi/burger-menu

--- estate_agent_site/app.js
+++ estate_agent_site/app.js
@@ -13,3 +13,35 @@ jack.addEventListener('click', openNav);
 function openNav() {
         can.classList.toggle('show');
 };
+
+// toggle to drop-navi
+
+const toggle = document.querySelector('.navi-toggle');
+
+const dropNavi = document.querySelector('.drop-navi');
+
+const showPic = document.querySelector('.show-casepicture');
+////////////////////////////////
+
+toggle.addEventListener('click', openDropNav);
+
+// dropNavi.addEventListener('click', openDropNav);
+
+showPic.addEventListener('click', openDropNav);
+
+function openDropNav() {
+        dropNavi.classList.toggle('see');
+        // showPic.classList.toggle('move');
+};
+
+
+
+
+
+// window.addEventListener('scroll', function() {
+        
+//         const scr
+        
+//         console.log('Scrolled');
+
+// });
