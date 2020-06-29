4e1f58a3f4a22d34786acd6f96faa1b64236473d
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-06-29 16:59:20 +0100

    animations/scrol function update for menu/dropdown navi

--- estate_agent_site/app.js
+++ estate_agent_site/app.js
@@ -4,25 +4,20 @@ const arrow = document.querySelector('.arrow-dropdown');
 const uk = document.querySelector('.country-option');
 const jack = document.querySelector('.jack');
 
-
 arrow.addEventListener('click', openNav);
 uk.addEventListener('click', openNav);
 jack.addEventListener('click', openNav);
-    
 
 function openNav() {
-        can.classList.toggle('show');
+        can.classList.toggle('show');      
 };
 //////////////////////////////////
 
 // toggle to drop-navi function
 
 const toggle = document.querySelector('.navi-toggle');
-
 const dropNavi = document.querySelector('.drop-navi');
-
 const showPic = document.querySelector('.show-casepicture');
-
 
 toggle.addEventListener('click', openDropNav);
 
@@ -35,23 +30,24 @@ function openDropNav() {
 
 // Scroll function
 const dropscrollNavi = document.querySelector('.scroll-navi');
+const hamburger = document.querySelector('li.hamburger');
 
 window.addEventListener('scroll', function () {
-        
-
         let scrolled = window.scrollY;
-        
-        
 
         if (scrolled > 500) {
                 dropNavi.classList.add('hide-navi');
-                
-        } else if (scrolled < 500)  {
-                dropNavi.classList.remove('hide-navi')
+                hamburger.classList.add('show-ham');
+        } else if (scrolled < 500) {
+                dropNavi.classList.remove('hide-navi');
+                hamburger.classList.remove('show-ham');
         }
-
-        console.log(scrolled)
 });
 //////////////////////////////////////
+
+
+
+//////////////////////////////////////
+
 
 
