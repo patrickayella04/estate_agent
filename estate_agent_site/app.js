1f31ba6b04c04d006693d2c135e6c30ce31ff2dd
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-07-14 04:43:07 +0100

    search page navi

--- estate_agent_site/app.js
+++ estate_agent_site/app.js
@@ -3,7 +3,7 @@ const can = document.querySelector('.can');
 const arrow = document.querySelector('.arrow-dropdown');
 const uk = document.querySelector('.country-option');
 const jack = document.querySelector('.jack');
-
+ 
 arrow.addEventListener('click', openNav);
 uk.addEventListener('click', openNav);
 jack.addEventListener('click', openNav);
